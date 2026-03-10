e64f7644083566e076ddb82b6347f0d3c719b44a
Rajasekar <rajasekarworks@gmail.com>
2026-03-10 22:37:46 +0530

    Cix CI Chrome headless issue

--- src/main/java/com/amazon/automation/api/utils/DriverFactory.java
+++ src/main/java/com/amazon/automation/api/utils/DriverFactory.java
@@ -3,15 +3,31 @@ package com.amazon.automation.api.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     public static WebDriver driver;
 
     public static WebDriver initDriver() {
+
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Detect if running in CI environment
+        String ci = System.getenv("CI");
+
+        if (ci != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+
         return driver;
     }
 }
